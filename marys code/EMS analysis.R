6f3f97b6bbf46450fbd08163f28722ff1808dd9c
Mary <oconnor@zoology.ubc.ca>
2016-05-17 17:30:21 -0700

    loaded data

--- marys code/EMS analysis.R
+++ marys code/EMS analysis.R
@@ -9,4 +9,25 @@
 #' 
 #' 5/17/2016 drafted code
 #'
-######################################################
+######################################################## 
+
+
+## load libraries
+
+library(metacom)
+library(plyr)
+library(dplyr)
+library(broom)
+library(reshape2)
+library(Matrix)
+library(lattice)
+
+## load data
+subsite <- read.csv("../species data/ZEN_2014_subsitesummary_2016_05_17.csv")
+head(subsite)
+
+plot <- read.csv("../species data/ZEN_2014_plot_2016_05_17.csv")
+head(plot)
+
+ZENverts <- read.csv("../Emmett files/ZENverts.v8.csv")
+head(ZENVerts)
