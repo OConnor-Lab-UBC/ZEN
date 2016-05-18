b4aa2aa614c5f8598275d8cdd89184215fd54b83
Mary <oconnor@zoology.ubc.ca>
2016-05-17 17:20:20 -0700

    added file for EMS analysis

---
+++ marys code/EMS analysis.R
@@ -0,0 +1,12 @@
+######################################################
+#'
+#' @title  EMS analysis for ZEN data
+#'
+#' @author Mary O'Connor
+#' @author oconnor@zoology.ubc.ca
+#'
+#' @log
+#' 
+#' 5/17/2016 drafted code
+#'
+######################################################
