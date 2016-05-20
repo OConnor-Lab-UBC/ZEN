b4adbe154df45df1cab6a832367f0427348a3544
Mary <oconnor@zoology.ubc.ca>
2016-05-19 17:16:29 -0700

    got data organized and ran first metacommunity analysis for goodwin islands

--- marys code/Goodwin Islands.R
+++ marys code/Goodwin Islands.R
@@ -16,5 +16,31 @@ idata1 <- idata[,c(2,4,7,10,23)]
 
 ## sum across replicates
 idata2 <- ddply(idata1, .(month, year, species.name.revised), summarise, sum(total))
-  
-idata3 <- melt(idata2, id = 1:2, measure = 3)
+idata2$time.ID <- paste(idata2$month, idata2$year, sep = '.')
+ 
+idata3 <- melt(idata2, id = c(1,2,3,5), measure.vars = "..1")
+
+idata4 <- dcast(idata3, time.ID ~ species.name.revised, mean)
+
+idata4 <- as.data.frame(idata4[,-2])
+
+is.nan.data.frame <- function(x)
+  do.call(cbind, lapply(x, is.nan))
+idata4[is.nan.data.frame(idata4)] <- 0
+idata4[is.na(idata4)] <- 0
+rowSums(idata4[,-1]) -> idata4$totals
+idata5 <- idata4[-which(idata4$totals == "0"),] 
+rownames(idata5) <- idata5[,1]
+idata5 <- idata5[,-1]
+idata5[idata5 > 0] <- 1
+
+# For EMS analysis
+Metacommunity(idata5, verbose = TRUE, allowEmpty = TRUE) -> meta 
+
+## assuming this works, make a plot
+a <- as.data.frame(meta[1])
+
+pdf('GoodwinIslands.pdf', width = 7, height = 9)
+levelplot(as.matrix(a), col.regions=c(0,1), region = TRUE, colorkey=FALSE, ylab = '', xlab = '', main="GoodwinIslands",  border="black", scales = list(cex = c(0.5, 0.5), x = list(rot = c(90))))
+dev.off()
+
